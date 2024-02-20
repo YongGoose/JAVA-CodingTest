4dd8b75d76fc3fbdfc20ab8cf4a51245acba9afe
yongjunhong <kevin0928@naver.com>
2024-02-20 09:44:29 +0900

    알고스팟 / 타일링 / 80ms

---
+++ 알고스팟/Tiling.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] cache = new int[101];
+    static final int MOD = 1_000_000_007;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int c = Integer.parseInt(br.readLine());
+        dp();
+        while (c-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 1) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(cache[n]).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+
+    static void dp() {
+        cache[2] = 2;
+        cache[3] = 3;
+        for (int i = 4; i <= 100; i++) {
+            cache[i] = (cache[i - 1] + cache[i - 2]) % MOD;
+        }
+    }
+}
+
