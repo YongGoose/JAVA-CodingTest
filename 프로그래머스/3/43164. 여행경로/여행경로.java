c5d27cab7262f35aba26d771b79f61191fb57879
Yongjun Hong <kevin0928@naver.com>
2024-03-28 10:00:49 +0900

    [level 3] Title: 여행경로, Time: 10.76 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    PriorityQueue<String> queue;
+    boolean[] visited;
+    public String[] solution(String[][] tickets) {
+        queue = new PriorityQueue<>();
+        visited = new boolean[tickets.length];
+        dfs("ICN","ICN",tickets,1);
+        String[] answer = queue.peek().split(" ");
+        return answer;
+    }
+    public void dfs(String start, String route, String[][] tickets, int cnt){
+        if(cnt == tickets.length + 1){
+            queue.add(route);
+            return;
+        }
+        for(int i = 0; i < tickets.length; i++){
+            if(tickets[i][0].equals(start) && !visited[i]){
+                visited[i] = true;
+                dfs(tickets[i][1], route + " " + tickets[i][1] , tickets, cnt + 1);
+                visited[i] = false;
+            }
+        }
+        
+    }
+}
