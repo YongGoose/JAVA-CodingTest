c5a4f3738db1d42e4803b09105efe5f10a0d57f3
Yongjun Hong <kevin0928@naver.com>
2024-04-06 12:31:09 +0900

    [Gold IV] Title: 좋다, Time: 516 ms, Memory: 35740 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -2,13 +2,15 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int[][] array;
+    static int cnt;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int testCase = Integer.parseInt(st.nextToken());
         int[] numbers = new int[testCase];
-        int[][] array = new int[testCase][testCase];
+        array = new int[testCase][testCase];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < testCase; i++) {
             numbers[i] = Integer.parseInt(st.nextToken());
@@ -31,17 +33,31 @@ public class Main {
     public static boolean getResult(int[][] array, int number, int idx) {
         int length = array.length;
         for (int i = 0; i < length; i++) {
-            for (int j = i; j < length; j++) {
-                if (number == array[i][j] && !(i == j)) {
-                    if (idx == i || idx == j) {
-                        continue;
-                    }
-                    return true;
-                }
+
+            if (i == idx) {
+                continue;
             }
 
+            if (binarySearch(i, array[i].length - 1, i, number, idx)) {
+                return true;
+            }
         }
         return false;
     }
 
+    static boolean binarySearch(int left, int right, int idx, int key, int keyIdx) {
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (array[idx][mid] > key) {
+                right = mid - 1;
+            } else if (idx != mid && mid != keyIdx && array[idx][mid] == key) {
+                return true;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
 }
+
