11d0020a87d0564b4a501be529378cf9381e6d88
Yongjun Hong <kevin0928@naver.com>
2024-04-08 09:45:18 +0900

    [level 2] Title: H-Index, Time: 0.59 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -6,17 +6,14 @@ class Solution {
         return countH(citations);
     }
     public int countH(int[] citations){
-        int result = 0, index = 0;
-        while(index < citations.length) {
-            int value = citations[index];
-            if (citations.length - index >= value) {
-                System.out.println(value);
+        int result = 0;
+        for(int i = 0; i < citations.length; i++) {
+            int value = citations[i];
+            if (citations.length - i >= value) {
                 result = Math.max(result, value);
             } else {
-                System.out.println(citations.length - index);
-                result = Math.max(result ,citations.length - index);
+                result = Math.max(result, citations.length - i);
             }
-            index++;
         }
         return result;
     }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
 
 ### 성능 요약
 
-메모리: 70.4 MB, 시간: 0.86 ms
+메모리: 74.7 MB, 시간: 0.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 08일 09:36:28
+2024년 04월 08일 09:45:15
 
 ### 문제 설명
 
