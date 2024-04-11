3cfc5a591b29aef4f58f736b22d20f2c7f601190
Yongjun Hong <kevin0928@naver.com>
2024-04-11 09:48:16 +0900

    [level 2] Title: 소수 찾기, Time: 12.73 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    boolean[] visited;
+    Set<Integer> set = new HashSet<>();
+    public int solution(String numbers) {
+        int answer = 0;
+        visited = new boolean[numbers.length()];
+        dfs(numbers, "", 0);
+        
+        for(Integer num : set) {
+            if(isPrime(num)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+    public void dfs(String numbers, String num, int depth) {
+        int length = numbers.length();
+        if (depth > length) {
+            return;
+        }
+        
+        for(int i = 0; i < length; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                String parseNum = (num + numbers.charAt(i));
+                Integer newNumber = Integer.parseInt(parseNum);
+                set.add(newNumber);
+                dfs(numbers, parseNum, depth + 1);
+                visited[i] = false;
+            }            
+        }
+    }
+    
+    public boolean isPrime(int number) {
+        if(number <= 1) {
+            return false;
+        }
+        for(int i = 2; i < number; i++) {
+            if(number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
