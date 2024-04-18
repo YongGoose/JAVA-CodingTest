5cc78816ba048a4ea55a4819920a6482d1d0faf1
Yongjun Hong <kevin0928@naver.com>
2024-04-18 11:02:48 +0900

    [Gold IV] Title: 최단경로, Time: 768 ms, Memory: 116092 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,80 +1,86 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-public class Main{
-    static ArrayList[] l;
-    static int[] dist;
+public class Main {
+    static ArrayList<Node>[] nodes;
+    static int v, e, x;
+    static int[] resultArray;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(br.readLine()) - 1;
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(br.readLine());
 
-        l = new ArrayList[V];
-        dist = new int[V];
-
-        for(int i = 0; i<V; i++) {
-            l[i] = new ArrayList<Node>();
-            dist[i] = Integer.MAX_VALUE;
+        nodes = new ArrayList[v + 1];
+        resultArray = new int[v + 1];
+        for (int i = 1; i < v + 1; i++) {
+            nodes[i] = new ArrayList<>();
+            resultArray[i] = Integer.MAX_VALUE;
         }
-
-        for(int i = 0; i<E; i++){
+        for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
 
-            int u = Integer.parseInt(st.nextToken()) - 1;
-            int v = Integer.parseInt(st.nextToken()) - 1;
-            int w = Integer.parseInt(st.nextToken());
-
-            l[u].add(new Node(v, w));
+            nodes[start].add(new Node(end, value));
         }
 
-        dijkstra(K);
+        dijk();
 
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i<V; i++){
-            if(dist[i] == Integer.MAX_VALUE)
+        for (int j = 1; j < v + 1; j++) {
+            if (resultArray[j] == Integer.MAX_VALUE) {
                 sb.append("INF").append("\n");
-            else
-                sb.append(dist[i]).append("\n");
+            } else {
+                sb.append(resultArray[j]).append("\n");
+            }
         }
-
-        System.out.print(sb);
+        System.out.println(sb);
+        br.close();
     }
 
-    static void dijkstra(int start){
+    public static void dijk() {
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        dist[start] = 0;
-        pq.add(new Node(start, 0));
+        pq.add(new Node(x, 0));
+        boolean[] visited = new boolean[v + 1];
+        resultArray[x] = 0;
 
-        while(!pq.isEmpty()) {
-            Node now = pq.poll();
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
 
-            int len = l[now.v].size();
-            for(int i = 0; i<len; i++){
-                Node next = (Node)l[now.v].get(i);
+            if (!visited[node.end]) {
+                visited[node.end] = true;
+            }
 
-                if(dist[next.v]>now.w + next.w){
-                    dist[next.v] = now.w + next.w;
-                    pq.add(new Node(next.v, dist[next.v]));
+            for (Node gNode : nodes[node.end]) {
+                if (!visited[gNode.end] && resultArray[gNode.end] > resultArray[node.end] + gNode.value) {
+                    resultArray[gNode.end] = resultArray[node.end] + gNode.value;
+                    pq.add(new Node(gNode.end, resultArray[gNode.end]));
                 }
             }
         }
     }
-}
 
-class Node implements Comparable<Node>{
-    int v, w;
+    static class Node implements Comparable<Node> {
+        int end;
+        int value;
 
-    public Node(int v, int w){
-        this.v = v;
-        this.w = w;
-    }
+        public Node(int end, int value) {
+            this.end = end;
+            this.value = value;
+        }
 
-    @Override
-    public int compareTo(Node n){
-        return this.w - n.w;
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.value, o.value);
+        }
     }
 }
