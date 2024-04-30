cf9ec16a2725f7eea0aa83bc0981f9e1d4ecdcc9
Yongjun Hong <kevin0928@naver.com>
2024-04-30 10:11:05 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 352 ms, Memory: 23776 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -12,33 +12,29 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        numberArray = new int[n][m];
-        for (int i = 0; i < n; i++) {
+        numberArray = new int[n + 1][m + 1];
+        for (int i = 1; i < n + 1; i++) {
             String input = br.readLine();
-            for (int j = 0; j < m; j++) {
-                numberArray[i][j] = input.charAt(j) - '0';
+            for (int j = 1; j < m + 1; j++) {
+                numberArray[i][j] = input.charAt(j - 1) - '0';
             }
         }
         System.out.println(dp(n, m));
     }
 
     static int dp(int n, int m) {
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
+        int maxNumber = 0;
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
                 if (numberArray[i][j] == 1) {
                     numberArray[i][j] = Math.min(numberArray[i][j - 1], Math.min(numberArray[i - 1][j], numberArray[i - 1][j - 1])) + 1;
-
                 } else {
                     numberArray[i][j] = 0;
                 }
-            }
-        }
-        int maxNumber = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
                 maxNumber = Math.max(maxNumber, numberArray[i][j]);
             }
         }
+
         return maxNumber * maxNumber;
     }
 }
