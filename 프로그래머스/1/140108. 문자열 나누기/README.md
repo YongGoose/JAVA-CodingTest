dcb95ce84988248465ece597be9f377b2af1c2df
Yongjun Hong <kevin0928@naver.com>
2024-05-03 01:22:42 +0900

    [level 1] Title: 문자열 나누기, Time: 43.92 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/README.md
+++ 프로그래머스/1/140108. 문자열 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 나누기 - 140108 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108?language=java#) 
 
 ### 성능 요약
 
-메모리: 76.8 MB, 시간: 2.65 ms
+메모리: 86.7 MB, 시간: 43.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 03일 01:08:12
+2024년 05월 03일 01:22:39
 
 ### 문제 설명
 
--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -3,29 +3,28 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        Map<Character, Integer> map = new HashMap<>();
-        char other = '#';
-        int st = 0;
+
         char target = s.charAt(0);
+        int st = 0;
         int last = s.length();
-        int cnt = 0;
+        int diff = 0, same = 0;
         for (int i = st; i < last; i++) {
             if (i == st) {
                 target = s.charAt(st);
-                map.put(target, 1);
-            } else {
-                char now = s.charAt(i);
-                if (target == now) map.put(target, map.getOrDefault(target, 0) + 1);
-                else {
-                    cnt++;
-                    // map.put(other, map.getOrDefault(other, 0) + 1);
-                }
             }
-            if (map.get(target) == cnt) {
+            char now = s.charAt(i);
+            if (target == now) {
+                same++;
+            } else {
+                diff++;
+            }
+
+            System.out.println(same + " " + diff);
+            if (same == diff) {
                 st = i + 1;
                 answer += 1;
-                map = new HashMap<>();
-                cnt = 0;
+                diff = 0;
+                same = 0;
             } else if (i == last - 1) answer += 1;
         }
 
