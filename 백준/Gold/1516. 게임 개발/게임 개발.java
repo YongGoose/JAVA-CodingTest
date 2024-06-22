91a6f93108ee8d4b22b0712e9aeb8cec85c1287c
Yongjun Hong <kevin0928@naver.com>
2024-06-22 10:51:35 +0900

    [Gold III] Title: 게임 개발, Time: 236 ms, Memory: 21200 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -19,28 +17,25 @@ public class Main {
 		resultArray = new int[testCase + 1];
 		datas = new ArrayList[testCase + 1];
 		dataArray = new int[testCase + 1];
+
 		for (int i = 1; i < testCase + 1; i++) {
 			datas[i] = new ArrayList<>();
 		}
+
 		for (int i = 0; i < testCase; i++) {
 			st = new StringTokenizer(br.readLine());
 			dataArray[i + 1] = Integer.parseInt(st.nextToken());
-			while (true) {
-				String input = st.nextToken();
-				if (input.equals("-1")) {
-					break;
-				}
+			String input = st.nextToken();
+			while (!input.equals("-1")) {
 				datas[i + 1].add(Integer.parseInt(input));
+				input = st.nextToken();
 			}
 		}
-		for (int i = 1; i < testCase + 1; i++) {
-			dp(i);
-		}
 		StringBuilder sb = new StringBuilder();
-
 		for (int i = 1; i < testCase + 1; i++) {
-			sb.append(resultArray[i]).append("\n");
+			sb.append(dp(i)).append("\n");
 		}
+
 		System.out.println(sb);
 	}
 
