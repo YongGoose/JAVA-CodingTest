7102a26899a1ce8c5728cbdfe9708a8f1aa487be
Yongjun Hong <kevin0928@naver.com>
2024-06-28 16:25:51 +0900

    [level 3] Title: 섬 연결하기, Time: 0.78 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class Solution {
+    
+    private int[] parent;
+    public int solution(int n, int[][] costs) {
+        
+        parent = new int[n];
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        
+        PriorityQueue<Node> nodes = new PriorityQueue<>();
+        for(int[] cost : costs) {
+            nodes.offer(new Node(cost[0],cost[1],cost[2]));
+        }
+        
+        int result = 0;
+        for(int i = 0; i < costs.length; i++) {
+            Node node = nodes.poll();
+            
+            int to = node.to;
+            int from = node.from;
+            
+            if(findParent(to) != findParent(from)) {
+                union(to,from);
+                result += node.value;
+            }
+        }
+        return result;
+    }
+    
+    private void union(int first, int second) {
+        first = findParent(first);
+        second = findParent(second);
+        
+        if(first >= second) {
+            parent[second] = first;
+        } else {
+            parent[first] = second;
+        }
+    }
+    
+    private int findParent(int per) {
+        if(parent[per] == per) {
+            return per;
+        }
+        return findParent(parent[per]);
+    }
+}
+
+class Node implements Comparable<Node> {
+    int from;
+    int to;
+    int value;
+    
+    @Override
+    public int compareTo(Node o) {
+        return this.value - o.value;
+    }
+    
+    public Node (int from, int to, int value) {
+        this.from = from;        
+        this.to = to;
+        this.value = value;
+    }
+}
