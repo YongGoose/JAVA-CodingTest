6c8cad12eee0777134751f3feb5984e820f90e35
Yongjun Hong <kevin0928@naver.com>
2024-06-28 13:59:35 +0900

    [level 2] Title: 기능개발, Time: 1.34 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Work> queue = new LinkedList<>();
+        
+        for(int i = 0; i < progresses.length; i++) {
+            queue.offer(new Work(progresses[i], speeds[i]));
+        }
+        
+        ArrayList<Integer> result = new ArrayList<>();
+		while(!queue.isEmpty()) {
+			if(queue.peek().progress >= 100) {
+				int cnt = 1;
+				queue.poll();
+
+				if (queue.isEmpty()) {
+					result.add(cnt);
+					break;
+				}
+				while(!queue.isEmpty()) {
+					if(queue.peek().progress >= 100) {
+						queue.poll();
+						cnt++;
+					} else {
+						break;
+					}
+				}
+				result.add(cnt);
+			}
+			queue.forEach(Work::plusSpeed);
+		}
+        int[] answer = new int[result.size()];
+        
+        int idx = 0;
+        for(Integer res : result) {
+            answer[idx++] = res;
+        }
+        return answer;
+    }
+}
+
+class Work {
+    int progress;
+    int speed;
+    
+    public Work(int progress, int speed) {
+        this.progress = progress;
+        this.speed = speed;
+    }
+    
+    public void plusSpeed() {
+        progress += speed;
+    }
+}
