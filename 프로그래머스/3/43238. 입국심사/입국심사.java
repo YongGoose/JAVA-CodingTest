ba50dfd9094a0eeec2001be7f7de362ec9033781
Yongjun Hong <kevin0928@naver.com>
2024-10-14 13:04:37 +0900

    [level 3] Title: 입국심사, Time: 60.69 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,28 +1,34 @@
-import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
-        Arrays.sort(times);
-        return binarySearch(1, (long) times[times.length - 1] * n, n, times);
-    }
-    public long binarySearch(long left, long right, int n, int[] times){
+        long maxValue = 0;
+        for(int time : times) {
+            maxValue = Math.max(maxValue, time);
+        }
+        
+        long start = 0, end = n * maxValue;
+        System.out.println(end);
         long answer = 0;
-        while (left <= right){            
-            long mid = (left + right) / 2;
+        while(start <= end) {
+            long mid = (start + end) / 2;
+            long res = compareTime(mid, times);
             
-            if(countPerson(mid, times) < n){
-                left = mid + 1;
-            } else {
+            if(res >= n) {
                 answer = mid;
-                right = mid - 1;
+                end = mid - 1;
+                continue;
             }
+            start = mid + 1;
         }
         return answer;
     }
-    public long countPerson(long mid, int[] times){
-        long cnt = 0;
-        for(int time : times){
-            cnt += (mid / time);
+    
+    private long compareTime(long t, int[] times) {
+        long result = 0;
+        
+        for(int time : times) {
+            result += t / time;
         }
-        return cnt;
+        
+        return result;
     }
 }
